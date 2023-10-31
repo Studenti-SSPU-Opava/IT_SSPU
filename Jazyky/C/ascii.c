050588c5173c40663fa09ef4d7ab6392e7057738
fpluske <frantisek.pluske@seznam.cz>
2023-10-31 12:26:13 +0100

    V 1.2

--- Jazyky/C/ascii.c
+++ Jazyky/C/ascii.c
@@ -4,8 +4,13 @@
 #define ZACATEK 14 //definuje konstantu ZACATEK
 
 int main() {
+    int opakovani = 0;
     for (int prvek = ZACATEK; prvek < POCETPRVKU; ++prvek) { //nastavi začátek pomocí konstanty ZACATEK, po každém opakování prvek++. Opakuje kód, pokud je prvek < POCETPRVKU
-        printf("%4d %c",prvek,prvek); //vypíše proměnnou prvek jako číslo, poté jako znak (tzn. znak ASCII)
+        printf("%10d %c",prvek,prvek); //vypíše proměnnou prvek jako číslo, poté jako znak (tzn. znak ASCII)
+        opakovani++;
+        if(opakovani % POCETSLOUPCU ==0) {
+            printf("\n");
+        }
     }
     return 0;
 }
