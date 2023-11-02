599c2a6463861bd1519081cd4a6fdeec3387a299
fpluske <frantisek.pluske@seznam.cz>
2023-11-02 12:23:11 +0100

    V 0.1

--- Jazyky/C/pin.c
+++ Jazyky/C/pin.c
@@ -5,18 +5,26 @@
 
 int main() {
     int pin;
-    int     countOfChar;
+    int countOfChar;
     int pokusy = POCETPOKUSU;
-    printf("Zadej pin.\n");
-    scanf("%d", &pin);
-    countOfChar = 0;
-    while(getchar() != '\n') {
-        countOfChar++;
-    }
-    pokusy--;
-    if(countOfChar != 0) {
-        printf("Zadal jsi nespravny PIN.\n");
-    }
-    else if()
+    do {
+        printf("Zadej pin.\n");
+        scanf("%d", &pin);
+        countOfChar = 0;
+        while (getchar() != '\n') {
+            countOfChar++;
+        }
+        pokusy--;
+        if (countOfChar != 0) {
+            printf("Zadal jsi nespravny PIN.\n");
+            pin = 0;
+        }
+        else if (pin == PIN) {
+            printf("Zadal jsi spravny PIN.\n");
+        }
+        else {
+            printf("Zadal jsi nespravny PIN.\n");
+        }
+    } while (pokusy > 0 && pin != PIN);
     return 0;
 }
