28e6274cb45e8a9949d4f7415172802dba6620bc
fpluske <frantisek.pluske@seznam.cz>
2023-11-28 21:36:54 +0100

    updateFiles

--- Jazyky/C/nabidka.c
+++ Jazyky/C/nabidka.c
@@ -3,7 +3,8 @@
 
 #define POCETVOLEB 6
 
-int main() {
+int main()
+{
     int volba;
     int countOfChar;
     float r = 2.54;
@@ -14,41 +15,47 @@ int main() {
     printf("4 - objem koule\n");
     printf("5 - povrch koule\n");
     printf("6 - konec programu\n");
-    do {
+    do
+    {
         printf("Zadejte volbu v intervalu 1-%d:", POCETVOLEB);
         scanf("%d", &volba);
         countOfChar = 0;
-        while (getchar() != '\n') {
+        while (getchar() != '\n')
+        {
             countOfChar++;
         }
-        if (countOfChar != 0) {
-            //nastaveni proměnné volba na neplatnou hodnotu
+        if (countOfChar != 0)
+        {
+            // nastaveni proměnné volba na neplatnou hodnotu
             volba = POCETVOLEB + 1;
         }
-        if (volba < 1 || volba > POCETVOLEB) {
+        if (volba < 1 || volba > POCETVOLEB)
+        {
             printf("Chybna volba, zadej znovu.\n");
         }
     } while (volba < 1 || volba > POCETVOLEB);
-    switch (volba) {
-        case 1:
-            printf("Hodnota polomeru %.2f je %.2f.\n", r, r);
-            break;
-        case 2:
-            printf("Obvod o polomeru %.2f je %.2f.\n", r, 2 * M_PI * r);
-            break;
-        case 3:
-            printf("Obsah o polomeru %.2f je %.2f.\n", r, M_PI * r * r);
-            break;
-        case 4:
-            printf("Objem o polomeru %.2f je %.2f.\n", r, 4.0 / 3.0 * M_PI * r * r * r);
-            break;
-        case 5:
-            printf("Povrch o polomeru %.2f je %.2f.\n", r, 4 * M_PI * r * r);
-            break;
-        case 6:
-            printf("Program bude ukoncen.\n");
-            break;
-        default:
+    switch (volba)
+    {
+    case 1:
+        printf("Hodnota polomeru %.2f je %.2f.\n", r, r);
+        break;
+    case 2:
+        printf("Obvod o polomeru %.2f je %.2f.\n", r, 2 * M_PI * r);
+        break;
+    case 3:
+        printf("Obsah o polomeru %.2f je %.2f.\n", r, M_PI * r * r);
+        break;
+    case 4:
+        printf("Objem o polomeru %.2f je %.2f.\n", r, 4.0 / 3.0 * M_PI * r * r * r);
+        break;
+    case 5:
+        printf("Povrch o polomeru %.2f je %.2f.\n", r, 4 * M_PI * r * r);
+        break;
+    case 6:
+        printf("Program bude ukoncen.\n");
+        break;
+    default:
     }
-    return 0;
+}
+return 0;
 }
