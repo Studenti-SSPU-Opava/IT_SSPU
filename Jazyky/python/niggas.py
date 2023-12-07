c1e106b3a6def5c7eb553f2cb60457156178fada
František Rubáček <100533721+Ferenc1234@users.noreply.github.com>
2023-12-07 06:37:43 +0000

    edit

--- Jazyky/python/niggas.py
+++ Jazyky/python/niggas.py
@@ -1,22 +1,39 @@
+from random import seed
+from random import choice
+
 n = 1
+d = 1
+w = ""
+weatherTypes = ["Je krásný den", "Je nehezký den"]
+selectAction = 1
+dollars = 20
 val = 0
+
+
+
 while True:
-    if (n == 0):
-        print("Došli ti negři, tvé naděje na život jsou nulové (umřel jsi)")
-        break
-    print("Máte", n, "negrů")
-    print("Stiskněte 1 pro koupení negra")
-    print("Stiskněte 2 pro ubičování a následnou smrt negra")
-    try: 
-        val = int(input())
+    w = choice(weatherTypes)
+    print("(den", str(d) + ")\n" + str(w), "a Vy máte", dollars, "dolarů\nCo dnes hodláte dělat?")
+    try:
+        selectAction = int(input("Napište číslo:\n\n1 - koupit negry\n2 - Pracovat\n"))
     except:
-        print("Vypadá to že mistr otrokář neumí psát čísla, negři ho ubičovali a zabili (umřel jsi)")
-    if (val == 1):
-        n += 1
-        print("koupili jste si negra.")
-    elif (val == 2):
-        n -= 1
-        print("Ubičovali a zabili jste negra (zemřel).")
-    else:
         break
-print("konec hry")
+    match selectAction:
+        case 1:
+            if (n == 0):
+                print("Došli ti negři, tvé naděje na život jsou nulové (umřel jste)")
+                break
+            print("\nMáte", n, "negrů\n\nStiskněte 1 pro koupení negra\nStiskněte 2 pro ubičování a následnou smrt negra\n")
+            try: 
+                val = int(input())
+                if (val == 1):
+                    n += 1
+                    print("koupili jste si negra.")
+                elif (val == 2):
+                    n -= 1
+                    print("Ubičovali a zabili jste negra (zemřel).")
+            except:
+                print("Vypadá to že mistr otrokář neumí psát čísla, negři ho ubičovali a zabili (umřel jste)")
+                break
+
+print("------------------KONEC HRY------------------")
