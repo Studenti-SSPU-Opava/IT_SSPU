4b2513c359abb2b36690cf4db9501c2e74c18ec5
František Rubáček <100533721+Ferenc1234@users.noreply.github.com>
2024-09-13 09:28:41 +0000

    bash

--- 1. ročník/jazyky/bash/prekladacZDatabaze.sh
+++ 1. ročník/jazyky/bash/prekladacZDatabaze.sh
@@ -5,7 +5,8 @@ function translate() {
     alias trsl="translate"
     if [ $# -eq 0 ]; then 
     echo "No parameters provided.";
-    else if [ $1 -eq 1 ]; then
+    fi
+    elif [ $1 -eq 1 ]; then
         awk -f
     fi
 }
