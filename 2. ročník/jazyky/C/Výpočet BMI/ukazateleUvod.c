99869949ce1d4c26f97f73bb9bacdf32d5c9cb25
Elias Constantine <147802118+Eliasko6562@users.noreply.github.com>
2024-09-26 09:05:19 +0000

    Okomentoval jsem kod

--- 2. ročník/jazyky/C/Výpočet BMI/ukazateleUvod.c
+++ 2. ročník/jazyky/C/Výpočet BMI/ukazateleUvod.c
@@ -1,16 +1,28 @@
+/*
+
+Uvod do ukazatelu a alokace pameti, 
+par experimentu s ukazateli a 
+minihra s polem.
+
+25.9.2024
+
+*/
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h> // Přidání knihovny pro funkci time()
 
 void pointersExperiment()
 {
     int number = 10;
     int *pointer = &number;
-    printf("This is the address of the number: %p\n"
-           "This is the value of the number: %d\n",
+    // Výpis adresy a hodnoty proměnné number
+    printf("Toto je adresa čísla: %p\n"
+           "Toto je hodnota čísla: %d\n",
            pointer, *pointer);
     int array[5] = {10, 20, 30, 40, 50};
-    printf("This is a failure executed successfully: %d\n"
-           "This is the way: %d\n",
+    // Výpis hodnot z pole array
+    printf("Toto je neúspěch provedený úspěšně: %d\n"
+           "Toto je cesta: %d\n",
            *array + 1, *(array + 1));
 }
 
@@ -18,31 +30,37 @@ int main()
 {
     int *pointer = NULL;
     int desiredLength;
-    printf("How long do you want the array to be?\n Enter: ");
+    // Dotaz na délku pole od uživatele
+    printf("Jak dlouhé chcete pole?\n Zadejte: ");
     scanf("%d", &desiredLength);
+    // Alokace paměti pro pole
     pointer = (int *)malloc(desiredLength * sizeof(int));
     if (pointer == NULL)
     {
-        printf("Bad pointer\n");
+        // Kontrola, zda alokace paměti byla úspěšná
+        printf("Špatný ukazatel\n");
         return 1;
     }
-    srand(time(NULL));
+    srand(time(NULL)); // Inicializace generátoru náhodných čísel
 
-    printf("Memory allocation was successful\n");
+    printf("Alokace paměti byla úspěšná\n");
     for (int i = 0; i < desiredLength; i++)
     {
+        // Generování náhodných hodnot a jejich uložení do pole
         pointer[i] = rand() % 1000 + 1;
-        printf("The address of the array position %d is: %p\n"
-               "Its value is: %d\n",
+        // Výpis adresy a hodnoty každé pozice v poli
+        printf("Adresa pozice pole %d je: %p\n"
+               "Její hodnota je: %d\n",
                i, &pointer[i], pointer[i]);
         if (pointer[i] == 1000)
         {
+            // Výpis zprávy při dosažení hodnoty 1000
             printf("------------------------------------------------------------------------------------------------------------------\n"
                    "--------------------------------------JACKPOOOOOOOOTTTTTTTT-------------------------------------------------------\n"
                    "------------------------------------------------------------------------------------------------------------------\n");
         }
     }
 
-    free(pointer);
+    free(pointer); // Uvolnění alokované paměti
     return 0;
 }
