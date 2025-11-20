f3375692e3bf99a3117c709a08b2f5a8ea672900
František Rubáček <100533721+Ferenc1234@users.noreply.github.com>
2025-11-20 09:30:11 +0000

    added code for zlomek.h

--- 3_rocnik/Programovani/Cpp/Priklady/zlomek/zlomek.cpp
+++ 3_rocnik/Programovani/Cpp/Priklady/zlomek/zlomek.cpp
@@ -1,33 +1,48 @@
-// Skeleton implementations for zlomek class.
-// All methods have empty bodies or placeholder return values.
-// Logic to be implemented later.
 
 #include "zlomek.h"
 
 zlomek::zlomek() : jmenovatel(1), citatel(1) {
 
 }
-zlomek::zlomek(int citatel, int jmenovatel) {
 
+
+
+zlomek::zlomek(int citatel, int jmenovatel) : jmenovatel(1), citatel(1) {
+    setZlomek(citatel, jmenovatel);
 }
+
+
+
 zlomek::zlomek(zlomek &kopirka) : citatel(kopirka.getCitatel()), jmenovatel(kopirka.getJmenovatel()) {
 
 }
+
+
+
 zlomek::~zlomek() {
 
 }
 
-int zlomek::getCitatel() const { 
-    
-return this->citatel; 
-}
-int zlomek::getJmenovatel() const { 
 
-return this->jmenovatel; 
+
+int zlomek::getCitatel() const { 
+    return this->citatel; 
 }
+
+
+
+int zlomek::getJmenovatel() const { 
+    return this->jmenovatel; 
+}
+
+
+
 void zlomek::setCitatel(int citatel) {
-this->citatel = citatel;
+    this->citatel = citatel;    
 }
+
+
+
 bool zlomek::setJmenovatel(int jmenovatel) {
     if (jmenovatel == 0) {
         return false;
@@ -36,26 +51,50 @@ bool zlomek::setJmenovatel(int jmenovatel) {
         return true;
     }
 }
+
+
+
 bool zlomek::setZlomek(int citatel, int jmenovatel) {
-
-return false; 
+    if (setJmenovatel(jmenovatel)) {
+        setCitatel(citatel);
+        return true;
+    } else {
+        return false;
+    }
 }
+
+
+
 float zlomek::desetinne() const {
-
-return 0.0f; 
+    return (float) citatel / (float) jmenovatel; 
 }
+
+
 bool zlomek::prevrat() {
-
-return false; 
+    if (getCitatel() != 0) {
+        swap(citatel, jmenovatel);
+        return true;
+    } else {
+        return false; 
+    }
 }
+
+
+
 bool zlomek::kraceni() {
-
-return false; 
+    
 }
+
+
 
 void zlomek::swap(int &a, int &b) {
-
+    int temp = a;
+    a = b;
+    b = temp;
 }
+
+
+
 int zlomek::spolecnyDelitel(int a, int b) {
 
 return 0; 
